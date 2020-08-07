11e281b36cb5a462f3d87229470f84e302eb5baa
Janani NA <janani2285@gmail.com>
2020-08-07 16:53:12 -0700

    Updates

---
+++ precache-manifest.ff5a77f6f886e27904212c675f8762b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3111317be7c50f20258d4881d52449a9",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "395d5665ec8eeecf3715",
+    "url": "/user-directory/static/js/2.764f553d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory/static/js/2.764f553d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93f0581ddd513c73b93",
+    "url": "/user-directory/static/js/main.82e65178.chunk.js"
+  },
+  {
+    "revision": "e0c8332cf67520a1e021",
+    "url": "/user-directory/static/js/runtime-main.2b603db7.js"
+  }
+]);
